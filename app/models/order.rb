d00b6e0950ef42208f660b50cd865301866e9f75
mariahsiao <thatsjustmeiwant@gmail.com>
2016-08-08 16:29:11 +0800

    gem "aasm" and connect with order.rb & orders_controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -34,4 +34,35 @@ class Order < ActiveRecord::Base
   def pay!
     self.update_columns(is_paid: true)
   end
+
+  include AASM
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment, after_commit: :pay! do
+      transitions form: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid,          to: :shipping
+    end
+
+    event :deliver do
+      transitions from: :shipping,      to: :shipped
+    end
+
+    event :returned_good do
+      transitions form: :shipped,       to: :good_returned
+    end
+
+    event :cancell_order do
+      transitions form: [:order_placed, :paid], to: :order_cancelled
+    end
+  end
 end
