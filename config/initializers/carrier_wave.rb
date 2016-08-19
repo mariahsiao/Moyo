42d718c75e9da1528f8f42419392c5c8f527fa92
mariahsiao <thatsjustmeiwant@gmail.com>
2016-08-19 19:30:39 +0800

    fix carrrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,15 +1,22 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage :fog
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['s3_key_id'],
-      aws_secret_access_key: ENV['s3_key_secret'],
-      region:                'ap-northeast-1',
-    }
-    config.fog_directory = 'moyotaiwan'
 
-  else
-    config.storage :file
+  if Rails.env.development?
+    CarrierWave.configure do |config|
+      config.storage :fog
+    end
   end
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage :fog
+    end
+  end
+
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['s3_key_id'],
+    aws_secret_access_key: ENV['s3_key_secret'],
+    region:                'ap-northeast-1',
+  }
+  config.fog_directory = 'moyotaiwan'
 end
